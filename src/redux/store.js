be0dc99a337f662ccc2844464c9906d367d5b76b
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-23 20:45:02 +0200

    added configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { configureStore } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from './contactsReducer';
 
 const persistConfig = {
